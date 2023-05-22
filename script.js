edf7f3a94c0f9edd99dcaac79012fac0326adb3a
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-05-22 12:34:45 -0400

    added comments for roadmap

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ var timeEl = document.querySelector(".time");
 // DATA 
 var secondsLeft = 10;
 // FUNCTIONS 
+// function to start game + add button to start game
 function startGame(){
     var startGame = prompt("Would you like to play a game?");
 }
+// function to start timer 
+// function - if timer ends = lose 
 function setTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -20,6 +23,11 @@ function setTime() {
         }
     }, 1000); // <-- number of miliseconds between intervals
 }
+
+// function to enter letters in blanks 
+// function if/else for win (if - all letters are input= win) (else - not all letters are input= lose)
+// function results to show wins and losses 
+// display message for when user wins or loses 
 
 //USER INTERACTIONS 
 // a user clicks start
